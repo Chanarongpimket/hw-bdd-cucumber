eae846dbda55b5305586968323cd7f45625e780a
Chanarongpimket <chanarongpimket@gmail.com>
2023-02-16 21:12:29 +0700

    update scanario

--- rottenpotatoes/features/filter_movie_list.feature
+++ rottenpotatoes/features/filter_movie_list.feature
@@ -20,30 +20,21 @@ Background: movies have been added to database
   | Chicken Run             | G      | 21-Jun-2000  |
 
   And  I am on the RottenPotatoes home page
+  Then 10 seed movies should exist
 
 Scenario: restrict to movies with 'PG' or 'R' ratings
   # enter step(s) to check the 'PG' and 'R' checkboxes
-  When I check the following ratings: PG  R
-
+  When I check the following ratings: PG, R
   # enter step(s) to uncheck all other checkboxes
-  And I uncheck the following ratings: G PG-13
-
+  When I uncheck the following ratings: G, PG-13, NC-17
   # enter step to "submit" the search form on the homepage
-  And I click the submit button
-
+  When I press "Refresh"
   # enter step(s) to ensure that PG and R movies are visible
-  Then I should see the following movies:
-
+  Then I should see the following movies: The Terminator, When Harry Met Sally, Amelie, The Incredibles, Raiders of the Lost Ark
   # enter step(s) to ensure that other movies are not visible
-  Then I should not see the following movies:
+  And I should not see the following movies: Aladdin, The Help, Chocolat, 2001: A Space Odyssey, Chicken Run
+
 
 Scenario: all ratings selected
   # see assignment
-  # enter step(s) to check all ratings
-  When I check all of the ratings
-
-  # enter step to "submit" the search form on the homepage
-  And I click the submit button
-
-  # enter step(s) to ensure that I see all movies
-  Then I should see all the movies
+  Then I should see all of the movies
--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -4,8 +4,12 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
-     Movie.create!(movie)
+    Movie.create(movie)
   end
+end
+
+Then /(.*) seed movies should exist/ do | n_seeds |
+  Movie.count.should be n_seeds.to_i
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -14,11 +18,22 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
- regexp = /#{e1}.*#{e2}/m
- page.body.should =~ regexp
-  
+  begin
+    # Check if e1 and e2 are dates.
+    DateTime.parse e1
+    DateTime.parse e2
 
+    within(:xpath, %(//table[@id="movies"]/tbody)) do
+      release_dates = page.all(:xpath, "//td[3]").to_a.map do 
+        |el| DateTime.parse(el.text)
+      end.compact
+      
+      expect(release_dates.index(e1) < release_dates.index(e2)).to be true
+    end
+  rescue ArgumentError
+    expect(page.body.index(e1) < page.body.index(e2)).to be true
   end
+end
 
 # Make it easier to express checking or unchecking several boxes at once
 #  "When I uncheck the following ratings: PG, G, R"
@@ -28,56 +43,20 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  my_ratings = rating_list.split
-  my_ratings.each do |field|
-  if uncheck == "un"
-    steps %Q{ When I uncheck "ratings_#{field}"
-    }
-  else
-    steps %Q{ When I check "ratings_#{field}"
-    }
+  rating_list.split /,\s*/ do |rating|
+    step %(I #{uncheck}check "ratings[#{rating}]")
   end
 end
-  
-  end
 
-Then /I should see all the movies/ do
+Then /I should see all of the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  steps %Q{ Then I should see "The Terminator"}
-  steps %Q{ Then I should see "When Harry Met Sally"}
-  steps %Q{ Then I should see "Amelie"}
-  steps %Q{ Then I should see "The Incredibles"}
-  steps %Q{ Then I should see "Raiders of the Lost Ark"}
-  steps %Q{ Then I should see "Aladdin"}
-  steps %Q{ Then I should see "The Help"}
-  steps %Q{ Then I should see "Chocolat"}
-  steps %Q{ Then I should see "2001: A Space Odyssey"}
-  steps %Q{ Then I should see "Chicken Run"}
+  expect(page).to have_xpath(".//tr[not(ancestor::thead)]", :count => Movie.count)
+end
+
+# Make it easier to express ensuring several movies are or are not visible at once
+
+Then /I should (not )?see the following movies: (.*)/ do |not_, movies|
+  movies.split /,\s*/ do |movie|
+    step %(I should #{not_}see "#{movie}")
   end
-
-When(/^I click the submit button$/) do 
-  steps %Q{When I press "ratings_submit"}
-end
-
-When(/^I check all of the ratings$/) do
-  steps %Q{ When I check "ratings_G"}
-  steps %Q{ When I check "ratings_PG"}
-  steps %Q{ When I check "ratings_PG-13"}
-  steps %Q{ When I check "ratings_R"}
-end
-
-Then(/^I should see the following movies:$/) do
-  steps %Q{ Then I should see "The Terminator"}
-  steps %Q{ Then I should see "When Harry Met Sally"}
-  steps %Q{ Then I should see "Amelie"}
-  steps %Q{ Then I should see "The Incredibles"}
-  steps %Q{ Then I should see "Raiders of the Lost Ark"}
-end
-
-Then(/^I should not see the following movies:$/) do
-  steps %Q{ Then I should not see "Aladdin"}
-  steps %Q{ Then I should not see "The Help"}
-  steps %Q{ Then I should not see "Chocolat"}
-  steps %Q{ Then I should not see "2001: A Space Odyssey"}
-  steps %Q{ Then I should not see "Chicken Run"}
 end
